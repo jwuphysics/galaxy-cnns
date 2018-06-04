4a4d38dea92122e6314b2d26bbb0f7470cccf84a
Steven Boada <stevenboada@gmail.com>
2018-06-04 10:59:06 -0400

    fixed to work with itertuples
    gotta preserve those datatypes!

--- download_images.py
+++ download_images.py
@@ -2,7 +2,6 @@ import skimage.io
 import pandas as pd
 import time
 import sys
-from numpy import int64
 import os
 import urllib
 
@@ -24,24 +23,23 @@ pixelsize = 0.396
 # total number of images
 n_gals = df.shape[0]
 
-for index, row in df.iterrows():
+for row in df.itertuples():
     # the 'scale' parameter is set so that the image will be about 2x the size
     # of the galaxy
-    scale = 2 * row['petroR90_r'] / pixelsize / width
+    scale = 2 * row.petroR90_r / pixelsize / width
     url = ("http://skyserver.sdss.org/dr14/SkyserverWS/ImgCutout/getjpeg"
            "?ra={}"
            "&dec={}"
            "&scale={}"
            "&width={}"
            "&height={}".format(row.ra, row.dec, scale, width, height))
-    if not os.path.isfile('images/{}.png'.format(row.objID.astype(int64))):
+    if not os.path.isfile('images/{}.png'.format(row.objID)):
         try:
             img = skimage.io.imread(url)
-            skimage.io.imsave('images/{}.png'.format(row.objID.astype(int64)), img)
+            skimage.io.imsave('images/{}.png'.format(row.objID), img)
             time.sleep(0.5)
         except urllib.error.HTTPError:
             pass
-    current = index / n_gals * 100
+    current = row.Index / n_gals * 100
     status = "{:.3f}% of {} completed.".format(current, n_gals)
     Printer(status)
-
